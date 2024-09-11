72217bb0414fd61e504f8d4de745070cfb8ce29a
Luke <lukebrown14@hotmail.com>
2024-09-11 10:46:19 +0100

    Added link to title to take you to home page

--- handlers/headlers.go
+++ handlers/headlers.go
@@ -12,7 +12,6 @@ func SearchRecipeHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	//err = utils.RenderHome(w, mealData.Meals[0])
 	err = utils.RenderTemplate(w, "recipe_list", mealData.Meals[0])
 	if err != nil {
 		log.Fatal(err)
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	http.HandleFunc("/", handlers.HomeHandler)
 	http.HandleFunc("/random-meal/", handlers.RandomRecipeHandler)
 	http.HandleFunc("/search-meal/", handlers.SearchRecipeHandler)
+	http.HandleFunc("/body/", handlers.BodyHandler)
 
 	log.Println("Server starting on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
